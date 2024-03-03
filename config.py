5bb45fc7527ed7fe7d4ccc5eebbd76d198c538d2
josephpitkin <jmpitkin@gmail.com>
2024-03-02 22:18:47 -0700

    Update.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 playlists = [
     {
-        'nickname': '',
+        'nickname': 'pitkin-apple-move',
         'applemusic_playlist_url': '',
         'spotify_playlist_id': '',
     },
@@ -10,8 +10,8 @@ playlists = [
 refresh_token = ''
 
 # https://developer.spotify.com/dashboard
-client_id = ''
-client_secret = ''
+client_id = 'c4da300a284c44c985bbdcb3080fb0f7'
+client_secret = '0c1aae62255f4b4a871eb9cf0a0c793e'
 
 # enable debug mode to see why songs are not being matched to eachother.
-debug = False
+debug = True
